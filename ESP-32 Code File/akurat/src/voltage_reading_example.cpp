9045837825a58d5f01f6129486cacde15985e1f8
Bayu Abi Pamungkas <bayuabi31@gmail.com>
2020-07-14 22:23:02 +0700

    tambah perhitungan tegangan baterai

---
+++ ESP-32 Code File/akurat/src/voltage_reading_example.cpp
@@ -0,0 +1,14 @@
+/*void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  uint16_t adc = analogRead(32);
+  Serial.println(adc);
+  Serial.println(linierCalc(adc, 0.0018493150684932, 0.2808904109589));
+  delay(1000);
+}
+
+float linierCalc(uint16_t adc, float slope, float intercept){
+  return ((float)adc * slope) + intercept;
+}*/
